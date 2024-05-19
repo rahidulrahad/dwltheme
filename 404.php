85a756497bfa75a426da57cb5fd32282cd3acbfd
rahidulrahad <rahidulislamrahad74@gmail.com>
2024-05-19 22:00:01 +0600

    error page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php  get_header(); ?>
+    <!-- Error Page -->
+    <section class="error-page section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Error Inner -->
+                    <div class="error-inner">
+                        <h1>404<span>Oop's  sorry we can't find that page!</span></h1>
+                        <p>Aenean eget sollicitudin lorem, et pretium felis. Nullam euismod diam libero, sed dapibus leo laoreet ut. Suspendisse potenti. Phasellus urna lacus</p>
+                    </div>
+                    <!--/ End Error Inner -->
+                </div>
+            </div>
+        </div>
+    </section>	
+    <!--/ End Error Page -->
+<?php get_footer(); ?>
